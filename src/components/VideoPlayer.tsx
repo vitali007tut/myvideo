a7705504a87d0c82620893796465e5d9eb5e05e2
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-03-01 17:09:22 +0300

    init

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,30 +1,79 @@
-import dynamic from 'next/dynamic';
-import React from 'react';
-import YouTube from 'react-youtube';
+// import React from 'react';
+// import YouTube from 'react-youtube';
+// // import KinescopePlayer from '@kinescope/react-kinescope-player';
+// import dynamic from 'next/dynamic';
+// const KinescopePlayer = dynamic(() => import('@kinescope/react-kinescope-player'), { ssr: false });
 
+// interface Props {
+//     videoId: string;
+//     type: 'kinescope' | 'youtube';
+// }
+
+// const VideoPlayer: React.FC<Props> = ({ videoId, type }) => {
+//     if (type === 'kinescope') {
+//         return <KinescopePlayer videoId={videoId} />;
+//     } else if (type === 'youtube') {
+//         const opts = {
+//             width: '100%',
+//             height: '100%',
+//             playerVars: {
+//                 autoplay: 1,
+//             },
+//         };
+
+//         return <YouTube videoId={videoId} opts={opts} />;
+//     }
+
+//     return null;
+// };
+
+// export default VideoPlayer;
+
+import React from 'react';
+import ReactPlayer from 'react-player';
+import dynamic from 'next/dynamic';
 const KinescopePlayer = dynamic(() => import('@kinescope/react-kinescope-player'), { ssr: false });
 
-interface Props {
-    videoId: string;
-    type: 'kinescope' | 'youtube';
-}
+type Props = {
+    url: string;
+    onStart?: () => void;
+    onAlmostPlayed?: () => void;
+};
 
-const VideoPlayer: React.FC<Props> = ({ videoId, type }) => {
-    if (type === 'kinescope') {
-        return <KinescopePlayer videoId={videoId} />;
-    } else if (type === 'youtube') {
-        const opts = {
-            width: '100%',
-            height: '100%',
-            playerVars: {
-                autoplay: 1,
-            },
-        };
+const VideoPlayer = ({ url }: Props) => {
+    const isKinescopeVideo = url.includes('kinescope.io');
 
-        return <YouTube videoId={videoId} opts={opts} />;
-    }
+    const extractKinescopeVideoId = (url: any) => {
+        const regex = /kinescope\.io\/(.+)/;
+        const match = url.match(regex);
+        return match ? match[1] : null;
+    };
 
-    return null;
+    const videoId = isKinescopeVideo ? extractKinescopeVideoId(url) : null;
+
+    return isKinescopeVideo && videoId ? (
+        <KinescopePlayer videoId={videoId} />
+    ) : (
+        <ReactPlayer
+            controls={true}
+            url={url}
+            width="100%"
+            height="auto"
+            config={{
+                youtube: {
+                    embedOptions: {
+                        height: '400px',
+                    },
+                },
+                vimeo: {
+                    playerOptions: {
+                        responsive: true,
+                        colors: ['000000', '00ADEF', 'FFFFFF', '18171a'],
+                    },
+                },
+            }}
+        />
+    );
 };
 
 export default VideoPlayer;
